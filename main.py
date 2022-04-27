af940d0b56f9d49b3da06640ce6a65a08c985926
mordy <imky171@gmail.com>
2022-04-27 18:18:21 -0400

    Basic starter

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from deta import Deta
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, render_template, request
 
 load_dotenv()
 app = Flask(__name__)
 deta = Deta()
 users = deta.Base('users')
+bookshelves = deta.Base('bookshelves')
 
 @app.route('/')
 def index():
-    return f'{("bob", "bob@gmail.com", "1234")}'
+    return render_template('index.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+    return render_template('login_signup.html', verb='Log In')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+    return render_template('login_signup.html', verb='Sign Up')
+
+if __name__ == '__main__':
+    app.run(debug=True)
